86a43b8e1a42a890ebb23e74c6cbb9a49b49fd43
rok1337 <facugiovanella88@gmail.com>
2025-01-03 22:11:01 -0300

    Primer proyecto en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
 
 @Component({
   selector: 'app-root',
+  //template: '<h1>My App</h1>',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  //styles: ['']
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'Luis';
+  imgURL = 'https://angular.io/assets/images/logos/angular/angular.png';
+  
+  getName() {
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgURL = (e.target as HTMLInputElement).value
+  }
+
+  logImg(event: string){
+    console.log(event);
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+  BrowserModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
